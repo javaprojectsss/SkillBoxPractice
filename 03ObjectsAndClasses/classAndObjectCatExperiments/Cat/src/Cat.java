ed0cecc725763e0262022656b76d6379019ece1f
Oleg <olesha_sha@List.ru>
2021-01-10 01:39:02 +0300

    class and object - cat experiments

---
+++ 03ObjectsAndClasses/classAndObjectCatExperiments/Cat/src/Cat.java
@@ -0,0 +1,63 @@
+
+public class Cat
+{
+
+    private double originWeight;
+    private double weight;
+
+    private double minWeight;
+    private double maxWeight;
+
+    public Cat()
+    {
+        weight = 1500.0 + 3000.0 * Math.random();
+        originWeight = weight;
+        minWeight = 999.0;
+        maxWeight = 9000.0;
+
+    }
+
+    public Double getMinWeight()
+
+    {
+
+        return minWeight;
+
+    }
+    public Double meow() {
+       weight = weight - 1;
+        return weight;
+        }
+
+
+    public void feed(Double amount)
+    {
+        weight = weight + amount;
+    }
+
+    public void drink(Double amount)
+    {
+        weight = weight + amount;
+    }
+
+    public Double getWeight()
+    {
+        return weight;
+    }
+
+    public String getStatus()
+    {
+        if(weight < minWeight) {
+            return "Dead";
+        }
+        else if(weight > maxWeight) {
+            return "Exploded";
+        }
+        else if(weight > originWeight) {
+            return "Sleeping";
+        }
+        else {
+            return "Playing";
+        }
+    }
+}
