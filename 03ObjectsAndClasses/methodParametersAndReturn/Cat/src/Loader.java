cbabf7617057be3e706138c65b2639adaef640a0
javaprojectsss <77177415+javaprojectsss@users.noreply.github.com>
2021-01-11 22:20:18 +0300

    mhetods, classes and return

--- 03ObjectsAndClasses/methodParametersAndReturn/Cat/src/Loader.java
+++ 03ObjectsAndClasses/methodParametersAndReturn/Cat/src/Loader.java
@@ -10,6 +10,6 @@ public class Loader {
         System.out.println("Введите число корма ещё раз ");
         catname1.feed((int) scanner.nextInt());
         System.out.println(catname1.randomName() + " Поел" + catname1.isaArtical() + " и стал" + catname1.isaArtical() + " весить " + catname1.getWeight());
-        System.out.println(catname1.pee() + catname1.getWeight());
+        System.out.println(catname1.pee() + " " + catname1.getWeight());
     }
 }
