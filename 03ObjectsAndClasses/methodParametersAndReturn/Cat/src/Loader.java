6d1862732fe70025661ff93e93540ade47d37310
javaprojectsss <77177415+javaprojectsss@users.noreply.github.com>
2021-01-11 22:46:28 +0300

    Mhetods, classes and return

--- 03ObjectsAndClasses/methodParametersAndReturn/Cat/src/Loader.java
+++ 03ObjectsAndClasses/methodParametersAndReturn/Cat/src/Loader.java
@@ -1,14 +1,22 @@
 import java.util.Scanner;
 
 public class Loader {
+
+
     public static void main(String[] args) {
-           Cat catname1 = new Cat();
-        System.out.println(catname1.randomName() + " весил" + catname1.isaArtical() + " " + catname1.getWeight());
+
         Scanner scanner = new Scanner(System.in);
+        Scanner scanner1 = new Scanner(System.in);
+
+        Cat catname1 = new Cat();
+        System.out.println(catname1.randomName() + " весил" + catname1.isaArtical() + " " + catname1.getWeight());
+
         System.out.println("Введите число корма ");
-           catname1.feed((int) scanner.nextInt());
-        System.out.println("Введите число корма ещё раз ");
         catname1.feed((int) scanner.nextInt());
+
+        System.out.println("Введите число корма ещё раз ");
+        catname1.feed((int) scanner1.nextInt());
+
         System.out.println(catname1.randomName() + " Поел" + catname1.isaArtical() + " и стал" + catname1.isaArtical() + " весить " + catname1.getWeight());
         System.out.println(catname1.pee() + " " + catname1.getWeight());
     }
