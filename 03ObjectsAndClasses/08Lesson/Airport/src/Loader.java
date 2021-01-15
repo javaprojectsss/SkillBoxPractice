c61b1cbf1f01f2fde536f3dce3c098be225ea1d9
Oleg <olesha_sha@List.ru>
2021-01-15 22:54:03 +0300

    airport

---
+++ 03ObjectsAndClasses/08Lesson/Airport/src/Loader.java
@@ -0,0 +1,14 @@
+import com.skillbox.airport.Aircraft;
+import com.skillbox.airport.Airport;
+
+public class Loader
+{
+
+    public static void main(String[] args)
+    {
+
+        Airport airport = Airport.getInstance();
+        System.out.println(airport.getAllAircrafts().size());
+
+    }
+}
