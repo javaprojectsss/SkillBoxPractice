6c994fe5e706f05086d75bf4499ab0c834f32001
Oleg <olesha_sha@List.ru>
2021-01-15 20:29:53 +0300

    kitten construction and objects

---
+++ 03ObjectsAndClasses/05Lesson/Cat/src/Cat.java
@@ -0,0 +1,70 @@
+import java.util.Scanner;
+
+public class Cat {
+    private double originWeight;
+    private double weight;
+
+    private double minWeight;
+    private double maxWeight;
+
+    private double cmdWeight = 0.0;
+
+    public Cat() {
+        weight = 1500.0 + 3000.0 * Math.random();
+        originWeight = weight;
+        minWeight = 1000.0;
+        maxWeight = 9000.0;
+
+    }
+
+    public Cat(double cmdWeight) {
+        this.cmdWeight = cmdWeight;
+
+    }
+
+    public double getCmdWeight()
+    {
+
+        return cmdWeight;
+
+    }
+
+
+
+    public void meow()
+    {
+        weight = weight - 1;
+        System.out.println("Meow");
+    }
+
+    public void feed(Double amount)
+    {
+        weight = weight + amount;
+    }
+
+    public void drink(Double amount)
+    {
+        weight = weight + amount;
+    }
+
+    public Double getWeight()
+    {
+        return weight;
+    }
+
+    public String getStatus()
+    {
+        if(weight < minWeight) {
+            return "Dead";
+        }
+        else if(weight > maxWeight) {
+            return "Exploded";
+        }
+        else if(weight > originWeight) {
+            return "Sleeping";
+        }
+        else {
+            return "Playing";
+        }
+    }
+}
---
+++ 03ObjectsAndClasses/05Lesson/Cat/src/Loader.java
@@ -0,0 +1,20 @@
+
+public class Loader
+{
+    public static void main(String[] args)
+    {
+
+        getKitten();
+        getKitten();
+        getKitten();
+
+    }
+
+    private static Cat getKitten() {
+        Cat cat = new Cat(1100.0);
+        System.out.println("Вес вашей кошки: " + cat.getCmdWeight());
+        return cat;
+
+    }
+
+}
